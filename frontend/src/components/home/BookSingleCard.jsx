4ef5cc9ff93f9266bb09991de80068faee5a3d1f
Neha_Kumari711 <nk8981398@gmail.com>
2024-06-20 20:44:09 +0530

    app url deploy process

--- frontend/src/components/home/BookSingleCard.jsx
+++ frontend/src/components/home/BookSingleCard.jsx
@@ -31,13 +31,13 @@ const BookSingleCard = ({ book }) => {
           className='text-3xl text-blue-800 hover:text-black cursor-pointer'
           onClick={() => setShowModal(true)}
         />
-        <Link to={`/books/details/${book._id}`}>
+        <Link to={`${config.VITE_APP_URL}/books/details/${book._id}`}>
           <BsInfoCircle className='text-2xl text-green-800 hover:text-black' />
         </Link>
-        <Link to={`/books/edit/${book._id}`}>
+        <Link to={`${config.VITE_APP_URL}/books/edit/${book._id}`}>
           <AiOutlineEdit className='text-2xl text-yellow-600 hover:text-black' />
         </Link>
-        <Link to={`/books/delete/${book._id}`}>
+        <Link to={`${config.VITE_APP_URL}/books/delete/${book._id}`}>
           <MdOutlineDelete className='text-2xl text-red-600 hover:text-black' />
         </Link>
       </div>
--- frontend/src/components/home/BooksTable.jsx
+++ frontend/src/components/home/BooksTable.jsx
@@ -2,6 +2,8 @@ import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
 import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import { config } from '../../config/config';
+
 
 const BooksTable = ({ books }) => {
   return (
@@ -37,13 +39,13 @@ const BooksTable = ({ books }) => {
             </td>
             <td className='border border-slate-700 rounded-md text-center'>
               <div className='flex justify-center gap-x-4'>
-                <Link to={`/books/details/${book._id}`}>
+                <Link to={`${config.VITE_APP_URL}/books/details/${book._id}`}>
                   <BsInfoCircle className='text-2xl text-green-800 hover:text-green-700' />
                 </Link>
-                <Link to={`/books/edit/${book._id}`}>
+                <Link to={`${config.VITE_APP_URL}/books/edit/${book._id}`}>
                   <AiOutlineEdit className='text-2xl text-yellow-600 hover:text-yellow-700' />
                 </Link>
-                <Link to={`/books/delete/${book._id}`}>
+                <Link to={`${config.VITE_APP_URL}/books/delete/${book._id}`}>
                   <MdOutlineDelete className='text-2xl text-red-600 hover:text-red-700' />
                 </Link>
               </div>
